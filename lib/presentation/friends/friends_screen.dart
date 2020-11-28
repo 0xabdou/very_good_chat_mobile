14b1bba80ca53cdaef1e29a51c3a41eb0f27383b
aouahib <ouahibdou@gmail.com>
2020-11-28 18:18:25 +0100

    Visual improvements for FriendsScreen

--- lib/presentation/friends/friends_screen.dart
+++ lib/presentation/friends/friends_screen.dart
@@ -1,6 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:very_good_chat/application/friends/friend_cubit.dart';
+import 'package:very_good_chat/domain/friends/friend.dart';
+import 'package:very_good_chat/presentation/shared/widgets/photo_placeholder.dart';
+import 'package:very_good_chat/shared/logger.dart';
 
 /// Friends screen
 class FriendsScreen extends StatelessWidget {
@@ -8,24 +12,100 @@ class FriendsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<FriendCubit, FriendState>(
       builder: (context, state) {
-        return ListView.builder(
-          itemCount: state.friends.length,
-          itemBuilder: (context, index) {
-            final friend = state.friends[index];
-            return ListTile(
-              leading: Image.network(friend.photoUrl),
-              title: Text(friend.username),
-              trailing: friend.isOnline
-                  ? Container(
-                      height: 5,
-                      width: 5,
-                      color: Colors.green,
-                    )
-                  : null,
-            );
-          },
+        return Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: ListView.separated(
+            itemCount: state.friends.length,
+            separatorBuilder: (context, index) {
+              return const Divider(
+                height: 0,
+                indent: 70,
+                endIndent: 16,
+                thickness: 0.15,
+                color: Colors.white,
+              );
+            },
+            itemBuilder: (context, index) {
+              return FriendsListItem(friend: state.friends[index]);
+            },
+          ),
         );
       },
+    );
+  }
+}
+
+/// List item for friends list
+@visibleForTesting
+class FriendsListItem extends StatelessWidget {
+  /// Constructor
+  const FriendsListItem({
+    Key key,
+    @required this.friend,
+  }) : super(key: key);
+
+  /// The friend for this list item
+  final Friend friend;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        logger.wtf('Go to conversation');
+      },
+      leading: SizedBox(
+        height: 40,
+        width: 40,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            CachedNetworkImage(
+              imageUrl: friend.photoUrl,
+              imageBuilder: (context, imageProvider) {
+                return Material(
+                  shape: const CircleBorder(),
+                  clipBehavior: Clip.antiAlias,
+                  child: Ink.image(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                    child: InkWell(
+                      onTap: () {
+                        logger.wtf('Go to profile');
+                      },
+                    ),
+                  ),
+                );
+              },
+              placeholder: (_, __) => ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: PhotoPlaceholder(),
+              ),
+            ),
+            if (friend.isOnline)
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  padding: const EdgeInsets.all(3),
+                  child: Container(
+                    height: 10,
+                    width: 10,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+              )
+          ],
+        ),
+      ),
+      title: Text(friend.username),
+      dense: true,
     );
   }
 }
---
+++ lib/presentation/shared/widgets/photo_placeholder.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+/// A place holder to display when loading photos
+class PhotoPlaceholder extends StatefulWidget {
+  @override
+  _PhotoPlaceholderState createState() => _PhotoPlaceholderState();
+}
+
+class _PhotoPlaceholderState extends State<PhotoPlaceholder>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<Color> _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    )
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed)
+          _controller.reverse();
+        else if (status == AnimationStatus.dismissed) _controller.forward();
+      })
+      ..forward();
+    final curve = CurvedAnimation(parent: _controller, curve: Curves.easeOut);
+    _animation = ColorTween(
+      begin: Colors.white.withOpacity(0),
+      end: Colors.white.withOpacity(0.2),
+    ).animate(curve);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, _) {
+        return Container(
+          color: _animation.value,
+        );
+      },
+    );
+  }
+}
