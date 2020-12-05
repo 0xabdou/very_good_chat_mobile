308524e45b4655d0a5be137451249913eeab49ea
aouahib <ouahibdou@gmail.com>
2020-12-05 18:20:56 +0100

    Add forgotten file to vc

---
+++ lib/application/auth/updating/updating_cubit.freezed.dart
@@ -0,0 +1,389 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'updating_cubit.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+class _$UpdatingStateTearOff {
+  const _$UpdatingStateTearOff();
+
+// ignore: unused_element
+  _UpdatingState call(
+      {@required String username,
+      @required String name,
+      bool callingApi = false,
+      bool done = false,
+      bool registering = false,
+      bool uploadingPhoto = false,
+      String authProviderAccessToken,
+      Uint8List photoBytes,
+      String photoUrl,
+      AuthFailure apiFailure,
+      String usernameError}) {
+    return _UpdatingState(
+      username: username,
+      name: name,
+      callingApi: callingApi,
+      done: done,
+      registering: registering,
+      uploadingPhoto: uploadingPhoto,
+      authProviderAccessToken: authProviderAccessToken,
+      photoBytes: photoBytes,
+      photoUrl: photoUrl,
+      apiFailure: apiFailure,
+      usernameError: usernameError,
+    );
+  }
+}
+
+/// @nodoc
+// ignore: unused_element
+const $UpdatingState = _$UpdatingStateTearOff();
+
+/// @nodoc
+mixin _$UpdatingState {
+  String get username;
+  String get name;
+  bool get callingApi;
+  bool get done;
+  bool get registering;
+  bool get uploadingPhoto;
+  String get authProviderAccessToken;
+  Uint8List get photoBytes;
+  String get photoUrl;
+  AuthFailure get apiFailure;
+  String get usernameError;
+
+  $UpdatingStateCopyWith<UpdatingState> get copyWith;
+}
+
+/// @nodoc
+abstract class $UpdatingStateCopyWith<$Res> {
+  factory $UpdatingStateCopyWith(
+          UpdatingState value, $Res Function(UpdatingState) then) =
+      _$UpdatingStateCopyWithImpl<$Res>;
+  $Res call(
+      {String username,
+      String name,
+      bool callingApi,
+      bool done,
+      bool registering,
+      bool uploadingPhoto,
+      String authProviderAccessToken,
+      Uint8List photoBytes,
+      String photoUrl,
+      AuthFailure apiFailure,
+      String usernameError});
+
+  $AuthFailureCopyWith<$Res> get apiFailure;
+}
+
+/// @nodoc
+class _$UpdatingStateCopyWithImpl<$Res>
+    implements $UpdatingStateCopyWith<$Res> {
+  _$UpdatingStateCopyWithImpl(this._value, this._then);
+
+  final UpdatingState _value;
+  // ignore: unused_field
+  final $Res Function(UpdatingState) _then;
+
+  @override
+  $Res call({
+    Object username = freezed,
+    Object name = freezed,
+    Object callingApi = freezed,
+    Object done = freezed,
+    Object registering = freezed,
+    Object uploadingPhoto = freezed,
+    Object authProviderAccessToken = freezed,
+    Object photoBytes = freezed,
+    Object photoUrl = freezed,
+    Object apiFailure = freezed,
+    Object usernameError = freezed,
+  }) {
+    return _then(_value.copyWith(
+      username: username == freezed ? _value.username : username as String,
+      name: name == freezed ? _value.name : name as String,
+      callingApi:
+          callingApi == freezed ? _value.callingApi : callingApi as bool,
+      done: done == freezed ? _value.done : done as bool,
+      registering:
+          registering == freezed ? _value.registering : registering as bool,
+      uploadingPhoto: uploadingPhoto == freezed
+          ? _value.uploadingPhoto
+          : uploadingPhoto as bool,
+      authProviderAccessToken: authProviderAccessToken == freezed
+          ? _value.authProviderAccessToken
+          : authProviderAccessToken as String,
+      photoBytes:
+          photoBytes == freezed ? _value.photoBytes : photoBytes as Uint8List,
+      photoUrl: photoUrl == freezed ? _value.photoUrl : photoUrl as String,
+      apiFailure:
+          apiFailure == freezed ? _value.apiFailure : apiFailure as AuthFailure,
+      usernameError: usernameError == freezed
+          ? _value.usernameError
+          : usernameError as String,
+    ));
+  }
+
+  @override
+  $AuthFailureCopyWith<$Res> get apiFailure {
+    if (_value.apiFailure == null) {
+      return null;
+    }
+    return $AuthFailureCopyWith<$Res>(_value.apiFailure, (value) {
+      return _then(_value.copyWith(apiFailure: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$UpdatingStateCopyWith<$Res>
+    implements $UpdatingStateCopyWith<$Res> {
+  factory _$UpdatingStateCopyWith(
+          _UpdatingState value, $Res Function(_UpdatingState) then) =
+      __$UpdatingStateCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String username,
+      String name,
+      bool callingApi,
+      bool done,
+      bool registering,
+      bool uploadingPhoto,
+      String authProviderAccessToken,
+      Uint8List photoBytes,
+      String photoUrl,
+      AuthFailure apiFailure,
+      String usernameError});
+
+  @override
+  $AuthFailureCopyWith<$Res> get apiFailure;
+}
+
+/// @nodoc
+class __$UpdatingStateCopyWithImpl<$Res>
+    extends _$UpdatingStateCopyWithImpl<$Res>
+    implements _$UpdatingStateCopyWith<$Res> {
+  __$UpdatingStateCopyWithImpl(
+      _UpdatingState _value, $Res Function(_UpdatingState) _then)
+      : super(_value, (v) => _then(v as _UpdatingState));
+
+  @override
+  _UpdatingState get _value => super._value as _UpdatingState;
+
+  @override
+  $Res call({
+    Object username = freezed,
+    Object name = freezed,
+    Object callingApi = freezed,
+    Object done = freezed,
+    Object registering = freezed,
+    Object uploadingPhoto = freezed,
+    Object authProviderAccessToken = freezed,
+    Object photoBytes = freezed,
+    Object photoUrl = freezed,
+    Object apiFailure = freezed,
+    Object usernameError = freezed,
+  }) {
+    return _then(_UpdatingState(
+      username: username == freezed ? _value.username : username as String,
+      name: name == freezed ? _value.name : name as String,
+      callingApi:
+          callingApi == freezed ? _value.callingApi : callingApi as bool,
+      done: done == freezed ? _value.done : done as bool,
+      registering:
+          registering == freezed ? _value.registering : registering as bool,
+      uploadingPhoto: uploadingPhoto == freezed
+          ? _value.uploadingPhoto
+          : uploadingPhoto as bool,
+      authProviderAccessToken: authProviderAccessToken == freezed
+          ? _value.authProviderAccessToken
+          : authProviderAccessToken as String,
+      photoBytes:
+          photoBytes == freezed ? _value.photoBytes : photoBytes as Uint8List,
+      photoUrl: photoUrl == freezed ? _value.photoUrl : photoUrl as String,
+      apiFailure:
+          apiFailure == freezed ? _value.apiFailure : apiFailure as AuthFailure,
+      usernameError: usernameError == freezed
+          ? _value.usernameError
+          : usernameError as String,
+    ));
+  }
+}
+
+/// @nodoc
+class _$_UpdatingState with DiagnosticableTreeMixin implements _UpdatingState {
+  const _$_UpdatingState(
+      {@required this.username,
+      @required this.name,
+      this.callingApi = false,
+      this.done = false,
+      this.registering = false,
+      this.uploadingPhoto = false,
+      this.authProviderAccessToken,
+      this.photoBytes,
+      this.photoUrl,
+      this.apiFailure,
+      this.usernameError})
+      : assert(username != null),
+        assert(name != null),
+        assert(callingApi != null),
+        assert(done != null),
+        assert(registering != null),
+        assert(uploadingPhoto != null);
+
+  @override
+  final String username;
+  @override
+  final String name;
+  @JsonKey(defaultValue: false)
+  @override
+  final bool callingApi;
+  @JsonKey(defaultValue: false)
+  @override
+  final bool done;
+  @JsonKey(defaultValue: false)
+  @override
+  final bool registering;
+  @JsonKey(defaultValue: false)
+  @override
+  final bool uploadingPhoto;
+  @override
+  final String authProviderAccessToken;
+  @override
+  final Uint8List photoBytes;
+  @override
+  final String photoUrl;
+  @override
+  final AuthFailure apiFailure;
+  @override
+  final String usernameError;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'UpdatingState(username: $username, name: $name, callingApi: $callingApi, done: $done, registering: $registering, uploadingPhoto: $uploadingPhoto, authProviderAccessToken: $authProviderAccessToken, photoBytes: $photoBytes, photoUrl: $photoUrl, apiFailure: $apiFailure, usernameError: $usernameError)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'UpdatingState'))
+      ..add(DiagnosticsProperty('username', username))
+      ..add(DiagnosticsProperty('name', name))
+      ..add(DiagnosticsProperty('callingApi', callingApi))
+      ..add(DiagnosticsProperty('done', done))
+      ..add(DiagnosticsProperty('registering', registering))
+      ..add(DiagnosticsProperty('uploadingPhoto', uploadingPhoto))
+      ..add(DiagnosticsProperty(
+          'authProviderAccessToken', authProviderAccessToken))
+      ..add(DiagnosticsProperty('photoBytes', photoBytes))
+      ..add(DiagnosticsProperty('photoUrl', photoUrl))
+      ..add(DiagnosticsProperty('apiFailure', apiFailure))
+      ..add(DiagnosticsProperty('usernameError', usernameError));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _UpdatingState &&
+            (identical(other.username, username) ||
+                const DeepCollectionEquality()
+                    .equals(other.username, username)) &&
+            (identical(other.name, name) ||
+                const DeepCollectionEquality().equals(other.name, name)) &&
+            (identical(other.callingApi, callingApi) ||
+                const DeepCollectionEquality()
+                    .equals(other.callingApi, callingApi)) &&
+            (identical(other.done, done) ||
+                const DeepCollectionEquality().equals(other.done, done)) &&
+            (identical(other.registering, registering) ||
+                const DeepCollectionEquality()
+                    .equals(other.registering, registering)) &&
+            (identical(other.uploadingPhoto, uploadingPhoto) ||
+                const DeepCollectionEquality()
+                    .equals(other.uploadingPhoto, uploadingPhoto)) &&
+            (identical(
+                    other.authProviderAccessToken, authProviderAccessToken) ||
+                const DeepCollectionEquality().equals(
+                    other.authProviderAccessToken, authProviderAccessToken)) &&
+            (identical(other.photoBytes, photoBytes) ||
+                const DeepCollectionEquality()
+                    .equals(other.photoBytes, photoBytes)) &&
+            (identical(other.photoUrl, photoUrl) ||
+                const DeepCollectionEquality()
+                    .equals(other.photoUrl, photoUrl)) &&
+            (identical(other.apiFailure, apiFailure) ||
+                const DeepCollectionEquality()
+                    .equals(other.apiFailure, apiFailure)) &&
+            (identical(other.usernameError, usernameError) ||
+                const DeepCollectionEquality()
+                    .equals(other.usernameError, usernameError)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(username) ^
+      const DeepCollectionEquality().hash(name) ^
+      const DeepCollectionEquality().hash(callingApi) ^
+      const DeepCollectionEquality().hash(done) ^
+      const DeepCollectionEquality().hash(registering) ^
+      const DeepCollectionEquality().hash(uploadingPhoto) ^
+      const DeepCollectionEquality().hash(authProviderAccessToken) ^
+      const DeepCollectionEquality().hash(photoBytes) ^
+      const DeepCollectionEquality().hash(photoUrl) ^
+      const DeepCollectionEquality().hash(apiFailure) ^
+      const DeepCollectionEquality().hash(usernameError);
+
+  @override
+  _$UpdatingStateCopyWith<_UpdatingState> get copyWith =>
+      __$UpdatingStateCopyWithImpl<_UpdatingState>(this, _$identity);
+}
+
+abstract class _UpdatingState implements UpdatingState {
+  const factory _UpdatingState(
+      {@required String username,
+      @required String name,
+      bool callingApi,
+      bool done,
+      bool registering,
+      bool uploadingPhoto,
+      String authProviderAccessToken,
+      Uint8List photoBytes,
+      String photoUrl,
+      AuthFailure apiFailure,
+      String usernameError}) = _$_UpdatingState;
+
+  @override
+  String get username;
+  @override
+  String get name;
+  @override
+  bool get callingApi;
+  @override
+  bool get done;
+  @override
+  bool get registering;
+  @override
+  bool get uploadingPhoto;
+  @override
+  String get authProviderAccessToken;
+  @override
+  Uint8List get photoBytes;
+  @override
+  String get photoUrl;
+  @override
+  AuthFailure get apiFailure;
+  @override
+  String get usernameError;
+  @override
+  _$UpdatingStateCopyWith<_UpdatingState> get copyWith;
+}
